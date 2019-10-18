011ce1d5c248a2e1a08b18c0aa0cab3799f681c5
Rahul Arya <r.rahularya96@gmail.com>
2019-10-18 17:21:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from utils import *
+from utilsv2 import *
 
-#### Start application 
-populatePlayList()
+def boot():
+    try:
+        #### Start application 
+        os.system("nohup python3 LiveCheck.py & > livestatus.out")
+        fetchAllImages()
+    except: 
+        print("Some error occured")
+        port = os.popen("""ps -ef | grep LiveCheck | grep -v grep | cut -d " " -f2""").read().split("\n")[0]
+        os.popen('kill -9 {}'.format(port))
+        test = os.listdir(LOCAL_PATH)
+        for item in test:
+            if item.endswith(IMAGE_FORMAT) or item.endswith(VIDEO_FORMAT) or item.endswith(NOHUP_FORMAT):
+                os.remove(os.path.join(LOCAL_PATH, item))
+        boot()
+
+boot()
